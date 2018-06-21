c9e49d311c1ebaad3452adf5db1ae78df61a919a
kennyho <kennyho.code@gmail.com>
2018-06-20 20:22:35 -0400

    commit 1

---
+++ test_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# echo "this is a shell script"
+
+alias hw="echo hello world"
+alias clr=clear
+alias gpush='git push origin $(git_current_branch)'
+alias o='open'
+alias ga.='git add .'
